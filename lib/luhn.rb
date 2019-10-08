16282840ade99a6c39676d6b2d800897a66e49c4
Ben Flaskrud <bflaskrud2@yahoo.com>
2019-10-08 04:20:59 +0000

    Initial commit

---
+++ lib/luhn.rb
@@ -0,0 +1,17 @@
+module Luhn
+  def self.is_valid?(number)
+    #WRITE YOUR SOLUTION HERE
+  cc_digits = number.to_s.reverse.chars.map(&:to_i)
+  check_sum = 0
+
+  cc_digits.each_slice(2) do |odd, even|
+    check_sum += odd
+    next unless even
+    even *= 2
+    even = even.divmod(10).inject(:+) if even > 9
+    check_sum += even
+  end
+
+  return check_sum.modulo(10) == 0
+  end
+end
